271bf2f306a42121be662c7d6f4ba183a3938298
imath <math.viet@gmail.com>
2020-01-25 20:51:41 +0100

    First try about the BP Member block
    1. Adds a Placeholder to select the member to insert
    2. Create a custom autocompleter to list the members according to what the admin typed into the text field of the placeholder
    3. Introduce the render callback function.
    
    See #3

--- build/bp-members/bp-members-blocks.php
+++ build/bp-members/bp-members-blocks.php
@@ -22,8 +22,87 @@ function bp_members_register_blocks() {
 			'name'               => 'bp/member',
 			'editor_script'      => 'bp-member-block',
 			'editor_script_url'  => plugins_url( 'js/blocks/member.js', __FILE__ ),
-			'editor_script_deps' => array( 'wp-blocks', 'wp-element' ),
+			'editor_script_deps' => array(
+				'wp-blocks',
+				'wp-element',
+				'wp-components',
+				'wp-i18n',
+				'wp-api-fetch',
+				'wp-editor',
+			),
+			'style'              => 'bp-member-block',
+			'style_url'          => plugins_url( 'css/blocks/member.css', __FILE__ ),
+			'render_callback'    => 'bp_members_render_member_block',
+			'attributes'         => array(
+				'memberID' => array(
+					'type'    => 'integer',
+					'default' => 0,
+				),
+			),
 		)
 	);
 }
 add_action( 'bp_blocks_init', 'bp_members_register_blocks', 10 );
+
+/**
+ * Callback function to render the BP Member Block.
+ *
+ * @since 6.0.0
+ *
+ * @param array $attributes The block attributes.
+ * @return string           HTML output.
+ */
+function bp_members_render_member_block( $attributes = array() ) {
+	$block_args = wp_parse_args(
+		$attributes,
+		array(
+			'memberID' => 0,
+		)
+	);
+
+	if ( ! $block_args['memberID'] ) {
+		return;
+	}
+
+	$member_id    = (int) $block_args['memberID'];
+	$username     = bp_core_get_username( $member_id );
+	$at_mention   = '';
+	$display_name = bp_core_get_user_displayname( $member_id );
+	$member_link  = bp_core_get_user_domain( $member_id );
+	$avatar       = bp_core_fetch_avatar(
+		array(
+			'item_id' => $member_id,
+			'object'  => 'user',
+			'type'    => 'full',
+			'html'    => false,
+		)
+	);
+
+	if ( bp_is_active( 'activity' ) && bp_activity_do_mentions() ) {
+		$at_mention = sprintf(
+			'<span class="user-nicename"><a href="%1$s">@%2$s</a></span>',
+			esc_url( $member_link ),
+			esc_html( $username )
+		);
+	}
+
+	return sprintf(
+		'<div class="bp-block-member">
+			<div class="item-header-avatar">
+				<a href="%1$s">
+					<img src="%2$s" alt="%3$s">
+				</a>
+			</div>
+			<div class="member-description">
+				<strong>%4$s</strong>
+				%5$s
+			</div>
+		</div>',
+		esc_url( $member_link ),
+		esc_url( $avatar ),
+		// Translators: %s is the member's display name.
+		sprintf( esc_html__( 'Profile photo of %s', 'buddypress' ), $display_name ),
+		esc_html( $display_name ),
+		$at_mention
+	);
+}
--- src/bp-members/js/blocks/member.js
+++ src/bp-members/js/blocks/member.js
@@ -1,6 +1,109 @@
 const { registerBlockType } = wp.blocks;
-const { createElement } = wp.element;
+const { Component, createElement } = wp.element;
+const { Placeholder, Button, Popover, Disabled } = wp.components;
+const { ServerSideRender } = wp.editor;
+const { apiFetch } = wp;
 const { __ } = wp.i18n;
+
+class MemberPlaceholder extends Component {
+	constructor() {
+		super( ...arguments );
+
+		this.state = {
+			search: '',
+			members: [],
+			error: '',
+		};
+
+		this.searchUsername = this.searchUsername.bind( this );
+		this.selectUsername = this.selectUsername.bind( this );
+	}
+
+	searchUsername( value ) {
+		const { search } = this.state;
+		this.setState( { search: value } );
+
+		if ( value.length < search.length ) {
+			this.setState( { members: [] } );
+		}
+
+		let payload = '';
+
+		if ( value ) {
+			payload = '?search=' + encodeURIComponent( value );
+		}
+
+		apiFetch( { path: '/buddypress/v1/members' + payload } ).then( members => {
+            this.setState( { members: members } );
+        }, error => {
+            this.setState( { error: error.message } );
+        } );
+	}
+
+	selectUsername( event, memberID ) {
+		const { onSelectMember } = this.props;
+		event.preventDefault();
+
+		this.setState( {
+			search: '',
+			members: [],
+			error: '',
+		} );
+
+		return onSelectMember( { memberID: memberID } );
+	}
+
+	render() {
+		const { search, members } = this.state;
+		let membersList;
+
+		if ( members.length ) {
+			membersList = members.map( ( member ) => {
+				return (
+					<button
+						type="button" key={ 'editor-autocompleters__member-item-' + member.id }
+						role="option"
+						aria-selected="true"
+						className="components-button components-autocomplete__result editor-autocompleters__user"
+						onClick={ ( event ) => this.selectUsername( event, member.id ) }
+					>
+						<img key="avatar" className="editor-autocompleters__user-avatar" alt="" src={ member.avatar_urls.thumb } />
+						<span key="name" className="editor-autocompleters__user-name">{ member.name }</span>
+						<span key="slug" className="editor-autocompleters__user-slug">{ member.mention_name }</span>
+					</button>
+				);
+			} );
+		}
+
+		return (
+			<Placeholder
+				icon="admin-users"
+				label={ __( 'BuddyPress Member', 'buddypress' ) }
+				instructions={ __( 'Start typing the name of the member you want to feature into this post.', 'buddypress' ) }
+			>
+				<input
+					type="text"
+					value={ search }
+					className="components-placeholder__input"
+					aria-label={ __( 'Member\'s username', 'buddypress' ) }
+					placeholder={ __( 'Enter Member\'s username here…', 'buddypress' ) }
+					onChange={ ( event ) => this.searchUsername( event.target.value ) }
+				/>
+				{ 0 !== members.length &&
+					<Popover
+						className="components-autocomplete__popover"
+						focusOnMount={ false }
+						position="bottom left"
+					>
+						<div className="components-autocomplete__results">
+							{ membersList }
+						</div>
+					</Popover>
+				}
+			</Placeholder>
+		);
+	}
+}
 
 registerBlockType( 'bp/member', {
 	title: __( 'Member', 'buddypress' ),
@@ -11,15 +114,28 @@ registerBlockType( 'bp/member', {
 
 	category: 'buddypress',
 
-	attributes: {},
-
-	edit: function( props ) {
-		return (
-			<p>{ __( 'Under construction', 'buddypress' )}</p>
-		);
+	attributes: {
+		memberID: {
+			type: 'integer',
+			default: 0,
+		}
 	},
 
-	save: function( props ) {
+	edit: function( { attributes, setAttributes } ) {
+		if ( ! attributes.memberID ) {
+			return (
+				<MemberPlaceholder onSelectMember={ setAttributes } />
+			);
+		}
+
+		return (
+			<Disabled>
+				<ServerSideRender block="bp/member" attributes={ attributes } />
+			</Disabled>
+		)
+	},
+
+	save: function() {
 		return null;
 	}
 } );
