77ba7566eaf93cf5d7b1557dd147184ab10056f2
imath <math.viet@gmail.com>
2020-02-11 22:09:13 +0100

    Build the output of the BP Group block
    See #2

--- build/bp-groups/bp-groups-blocks.php
+++ build/bp-groups/bp-groups-blocks.php
@@ -33,6 +33,8 @@ function bp_groups_register_blocks() {
 				'wp-data',
 				'wp-block-editor',
 			),
+			'style'              => 'bp-group-block',
+			'style_url'          => plugins_url( 'css/blocks/group.css', __FILE__ ),
 			'render_callback'    => 'bp_groups_render_group_block',
 			'attributes'         => array(
 				'itemID'              => array(
@@ -93,6 +95,8 @@ add_filter( 'bp_blocks_editor_settings', 'bp_groups_editor_settings' );
  * @return string           HTML output.
  */
 function bp_groups_render_group_block( $attributes = array() ) {
+	$bp = buddypress();
+
 	$block_args = wp_parse_args(
 		$attributes,
 		array(
@@ -115,5 +119,119 @@ function bp_groups_render_group_block( $attributes = array() ) {
 	// Group object.
 	$group = groups_get_group( $group_id );
 
-	return $group->id;
+	if ( ! $group->id ) {
+		return;
+	}
+
+	// Avatar variables.
+	$avatar           = '';
+	$avatar_container = '';
+
+	// Cover image variable.
+	$cover_image     = '';
+	$cover_container = '';
+
+	// Group name/link/description variables.
+	$group_name        = bp_get_group_name( $group );
+	$group_link        = bp_get_group_permalink( $group );
+	$group_description = '';
+
+	// Group action button.
+	$action_button         = '';
+	$display_action_button = (bool) $block_args['displayActionButton'];
+
+	if ( $bp->avatar && $bp->avatar->show_avatars && ! bp_disable_group_avatar_uploads() && in_array( $block_args['avatarSize'], array( 'thumb', 'full' ), true ) ) {
+		$avatar = bp_core_fetch_avatar(
+			array(
+				'item_id' => $group->id,
+				'object'  => 'group',
+				'type'    => $block_args['avatarSize'],
+				'html'    => false,
+			)
+		);
+
+		$container_classes[] = 'avatar-' . $block_args['avatarSize'];
+	} else {
+		$container_classes[] = 'avatar-none';
+	}
+
+	if ( $avatar ) {
+		$avatar_container = sprintf(
+			'<div class="item-header-avatar">
+				<a href="%1$s">
+					<img src="%2$s" alt="%3$s" class="avatar">
+				</a>
+			</div>',
+			esc_url( $group_link ),
+			esc_url( $avatar ),
+			// Translators: %s is the group's name.
+			sprintf( esc_html__( 'Group Profile photo of %s', 'buddypress' ), $group_name )
+		);
+	}
+
+	$display_cover_image = (bool) $block_args['displayCoverImage'];
+	if ( bp_is_active( 'groups', 'cover_image' ) && $display_cover_image ) {
+		$cover_image = bp_attachments_get_attachment(
+			'url',
+			array(
+				'item_id'    => $group->id,
+				'object_dir' => 'groups',
+			)
+		);
+
+		if ( $cover_image ) {
+			$cover_image = sprintf(
+				' style="background-image: url( %s );"',
+				esc_url( $cover_image )
+			);
+		}
+
+		$cover_container = sprintf(
+			'<div class="bp-group-cover-image"%s></div>',
+			$cover_image
+		);
+
+		$container_classes[] = 'has-cover';
+	}
+
+	$display_description = (bool) $block_args['displayDescription'];
+	if ( $display_description ) {
+		$group_description = sprintf(
+			'<div class="group-description-content">%s</div>',
+			bp_get_group_description( $group )
+		);
+
+		$container_classes[] = 'has-description';
+	}
+
+	if ( $display_action_button ) {
+		$action_button = sprintf(
+			'<div class="bp-profile-button">
+				<a href="%1$s" class="button large primary button-primary" role="button">%2$s</a>
+			</div>',
+			esc_url( $group_link ),
+			esc_html__( 'Visit Group', 'buddypress' )
+		);
+	}
+
+	return sprintf(
+		'<div class="%1$s">
+			%2$s
+			<div class="group-content">
+				%3$s
+				<div class="group-description">
+					<strong><a href="%4$s">%5$s</a></strong>
+					%6$s
+					%7$s
+				</div>
+			</div>
+		</div>',
+		implode( ' ', array_map( 'sanitize_html_class', $container_classes ) ),
+		$cover_container,
+		$avatar_container,
+		esc_url( $group_link ),
+		esc_html( $group_name ),
+		$group_description,
+		$action_button,
+	);
 }
