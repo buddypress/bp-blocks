610ed027d6f7b0a159a6927662f3a5670c5c529f
imath <imathi.eu@outlook.fr>
2020-06-09 21:25:41 +0200

    Firts try at building a button to insert emojis

--- src/bp-activity/js/blocks/block-types/bp-text.js
+++ src/bp-activity/js/blocks/block-types/bp-text.js
@@ -4,21 +4,73 @@
 const { registerBlockType, createBlock } = wp.blocks;
 const { useDispatch } = wp.data;
 const { RichText, BlockControls } = wp.blockEditor;
+const { insert, create, getTextContent } = wp.richText;
+const { Button, ToolbarGroup, Popover, Dashicon } = wp.components;
+const { renderToString, useState, useRef } = wp.element;
 const { __ } = wp.i18n;
+
+const getRange = () => {
+	const selection = window.getSelection();
+	return selection.rangeCount ? selection.getRangeAt( 0 ) : null;
+}
 
 const editText = ( { attributes, mergeBlocks, onReplace, clientId, setAttributes } ) => {
 	const { content } = attributes;
 	const name = 'bp/text';
 	const { removeBlock } = useDispatch( 'core/block-editor' );
 	const onRemove = () => removeBlock( clientId );
+	const [ isVisible, setVisible ] = useState( false );
+
+	// Used to track the current RichText.
+	const ref = useRef();
+
+	const setContent = ( content ) => {
+		setAttributes( { content: content } )
+	}
+
+	const showEmojiPicker = ( emoji ) => {
+		const anchor = getRange();
+		const valueToInsert = emoji;
+		const value = create( { html: renderToString( content ) } );
+
+		value.start = anchor.startOffset;
+		value.end   = anchor.endOffset;
+
+		const newContent = insert( value, valueToInsert );
+
+		/**
+		 * This needs to be improved as our anchor is not taking in account the
+		 * format applied to the text.
+		 */
+		setAttributes( { content: getTextContent( newContent ) } );
+
+		// Give the focus back to the RichText.
+		ref.current.focus();
+	}
 
 	return (
 		<>
-			<BlockControls/>
+			<BlockControls>
+				<ToolbarGroup>
+					<Button
+						className="components-dropdown-menu__toggle"
+						onClick={ setVisible }
+					>
+						<Dashicon icon="smiley"/>
+						{ isVisible && (
+							<Popover position="bottom center" onClose={ () => setVisible( false ) }>
+								<Button key="poop" onClick={ () => showEmojiPicker( '💩' ) }>💩</Button>
+								<Button key="houray" onClick={ () => showEmojiPicker( '🙌' ) }>🙌</Button>
+							</Popover>
+						) }
+					</Button>
+				</ToolbarGroup>
+			</BlockControls>
 			<RichText
 				identifier="content"
 				tagName="p"
-				onChange={ ( content ) => setAttributes( { content: content } ) }
+				ref={ ref }
+				onChange={ ( content ) => setContent( content ) }
 				value={ content }
 				onSplit={ ( value ) => {
 					if ( ! value ) {
