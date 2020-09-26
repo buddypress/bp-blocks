fb2bf5e68759edffd2c2f1191fe022ddbcdd27f7
imath <math.viet@gmail.com>
2020-09-26 21:27:00 +0200

    First step at building the BP Members Block
    The idea of this block is to let the post/page author to select the members he wants to add to the members list.
    
    The BP Autocompleter make it easy to select members one by one. If we are using a dynamic block, we can't use the ServerSideRender component to output the block.
    We need to deal with it using JavaScript so that it's possible to remove a member from the list.

--- build/bp-members/bp-members-blocks.php
+++ build/bp-members/bp-members-blocks.php
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Add new BP Members Blocks.
  *
- * @since 6.1.0
+ * @since 7.0.0
  *
  * @param array $blocks The list of BP Members blocks.
  * @return array The list of BP Members blocks.
@@ -30,13 +30,69 @@ function bp_blocks_register_member_blocks( $blocks ) {
 				'editor_script_deps' => array(
 					'wp-blocks',
 					'wp-element',
+					'wp-components',
 					'wp-i18n',
+					'wp-compose',
+					'wp-data',
+					'wp-api-fetch',
+					'wp-url',
+					'wp-block-editor',
+					'bp-block-components',
 				),
 				'style'              => 'bp-members-block',
 				'style_url'          => plugins_url( 'css/blocks/members.css', __FILE__ ),
-				'attributes'         => array(),
+				'attributes'         => array(
+					'itemIDs'            => array(
+						'type'  => 'array',
+						'items' => array(
+							'type' => 'integer',
+						),
+					),
+					'avatarSize'         => array(
+						'type'    => 'string',
+						'default' => 'full',
+					),
+					'displayMentionSlug' => array(
+						'type'    => 'boolean',
+						'default' => true,
+					),
+					'displayUserName'    => array(
+						'type'    => 'boolean',
+						'default' => true,
+					),
+					'displayCoverImage'  => array(
+						'type'    => 'boolean',
+						'default' => true,
+					),
+				),
+				'render_callback'    => 'bp_members_render_members_block',
 			),
 		)
 	);
 }
 add_filter( 'bp_members_register_blocks', 'bp_blocks_register_member_blocks', 10, 1 );
+
+/**
+ * Callback function to render the BP Members Block.
+ *
+ * @since 7.0.0
+ *
+ * @param array $attributes The block attributes.
+ * @return string           HTML output.
+ */
+function bp_members_render_members_block( $attributes = array() ) {
+	$bp = buddypress();
+
+	$block_args = wp_parse_args(
+		$attributes,
+		array(
+			'itemIDs'            => array(),
+			'avatarSize'         => 'full',
+			'displayMentionSlug' => true,
+			'displayUserName'    => true,
+			'displayCoverImage'  => true,
+		)
+	);
+
+	return sprintf( '<p>%s</p>', implode( ', ', $block_args['itemIDs'] ) );
+}
