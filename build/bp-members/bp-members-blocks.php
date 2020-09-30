59b2dcb92e912ec0c75ad224cfeff7178e0b6006
imath <math.viet@gmail.com>
2020-09-30 22:35:06 +0200

    Build the front-end output for the block

--- build/bp-members/bp-members-blocks.php
+++ build/bp-members/bp-members-blocks.php
@@ -96,8 +96,91 @@ function bp_members_render_members_block( $attributes = array() ) {
 			'avatarSize'         => 'full',
 			'displayMentionSlug' => true,
 			'displayUserName'    => true,
+			'layoutPreference'   => 'list',
+			'columns'            => '2',
 		)
 	);
 
-	return sprintf( '<p>%s</p>', implode( ', ', $block_args['itemIDs'] ) );
+	$member_ids = wp_parse_id_list( $block_args['itemIDs'] );
+	if ( ! array_filter( $member_ids ) ) {
+		return '';
+	}
+
+	$container_classes = sprintf( 'bp-block-members avatar-%s', $block_args['avatarSize'] );
+	if ( 'grid' === $block_args['layoutPreference'] ) {
+		$container_classes .= sprintf( ' is-grid columns-%d', (int) $block_args['columns'] );
+	}
+
+	$query = bp_core_get_users(
+		array(
+			'user_ids' => $member_ids,
+		)
+	);
+
+	// Initialize the output and the members.
+	$output  = '';
+	$members = $query['users'];
+
+	foreach ( $members as $member ) {
+		$output .= '<div class="member-content">';
+
+		// Get Member link.
+		$member_link = bp_core_get_user_domain( $member->ID );
+
+		// Set the Avatar output.
+		if ( $bp->avatar && $bp->avatar->show_avatars && 'none' !== $block_args['avatarSize'] ) {
+			$output .= sprintf(
+				'<div class="item-header-avatar">
+					<a href="%1$s">
+						<img class="avatar" alt="%2$s" src="%3$s" />
+					</a>
+				</div>',
+				esc_url( $member_link ),
+				/* translators: %s: the member display name */
+				sprintf( esc_attr__( 'Profile photo of %s', 'buddypress' ), $member->display_name ),
+				esc_url(
+					bp_core_fetch_avatar(
+						array(
+							'item_id' => $member->ID,
+							'object'  => 'user',
+							'type'    => $block_args['avatarSize'],
+							'html'    => false,
+						)
+					)
+				)
+			);
+		}
+
+		$output .= '<div class="member-description">';
+		if ( $block_args['displayUserName'] ) {
+			$output .= sprintf(
+				'<strong><a href="%1$s">%2$s</a></strong>',
+				esc_url( $member_link ),
+				esc_html( $member->display_name )
+			);
+		}
+
+		if ( bp_is_active( 'activity' ) && bp_activity_do_mentions() && $block_args['displayMentionSlug'] ) {
+			$output .= sprintf(
+				'<span class="user-nicename">@%s</span>',
+				esc_html( $member->user_nicename )
+			);
+		}
+
+		$output .= '</div></div>';
+	}
+
+	// Set the final output.
+	$output = sprintf( '<div class="%1$s">%2$s</div>', $container_classes, $output );
+
+	/**
+	 * Filter here to edit the output of the members block.
+	 *
+	 * @since 7.0.0
+	 *
+	 * @param string $output     The HTML output of the block.
+	 * @param array  $block_args The block arguments.
+	 * @param array  $members    The list of WP_User objects.
+	 */
+	return apply_filters( 'bp_members_render_member_block_output', $output, $block_args, $members );
 }
