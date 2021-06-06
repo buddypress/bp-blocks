0f65556d1e33314019cbc1d427061962cf55f6f3
imath <math.viet@gmail.com>
2021-06-06 05:45:21 +0200

    Add a rest field to the Components endpoint to fetch active features

--- build/bp-core/bp-core-blocks.php
+++ build/bp-core/bp-core-blocks.php
@@ -64,6 +64,68 @@ function bp_register_block_components() {
 add_action( 'bp_blocks_init', __NAMESPACE__ . '\bp_register_block_components', 1 );
 
 /**
+ * Gets information about active features for BP Components.
+ *
+ * @since 8.0.0
+ *
+ * @param array           $data     The list of properties of the BuddyPress component's object.
+ * @param string          $property The custom property being requested.
+ * @param WP_REST_Request $request  Full details about the request.
+ * @return array The information about active features for BP Components.
+ */
+function bp_blocks_get_rest_component_features( $data, $property, $request ) {
+	$features = array();
+
+	if ( 'features' === $property && isset( $data['name'] ) ) {
+		$component_id = $data['name'];
+		$bp           = buddypress();
+
+		if ( bp_is_active( 'groups' ) && $bp->groups->id === $data['name'] ) {
+			$features = array(
+				'avatar' => $bp->avatar && $bp->avatar->show_avatars && ! bp_disable_group_avatar_uploads(),
+				'cover'  => bp_is_active( 'groups', 'cover_image' ),
+			);
+		} elseif ( bp_is_active( 'members' ) && $bp->members->id === $data['name'] ) {
+			$features = array(
+				'avatar' => $bp->avatar && $bp->avatar->show_avatars,
+				'cover'  => bp_is_active( 'members', 'cover_image' ),
+			);
+		} elseif ( bp_is_active( 'activity' ) && $bp->activity->id ===  $data['name'] ) {
+			$features = array(
+				'mentions' => bp_activity_do_mentions(),
+			);
+		}
+	}
+
+	return $features;
+}
+
+/**
+ * Adds a new rest field to fetch features information about the BP Components.
+ *
+ * @since 8.0.0
+ */
+function bp_blocks_register_components_rest_field() {
+	/**
+	 * NB: bp_rest_register_field is only used for BP Components like:
+	 * `groups` or `members`.
+	 */
+	register_rest_field(
+		'bp_components',
+		'features',
+		array(
+			'get_callback' => __NAMESPACE__ . '\bp_blocks_get_rest_component_features',
+			'schema'       => array(
+				'description' => __( 'Information about active features for the component.', 'buddypress' ),
+				'type'        => 'array',
+				'context'     => array( 'view', 'edit' ),
+			),
+		)
+	);
+}
+add_action( 'bp_rest_api_init', __NAMESPACE__ . '\bp_blocks_register_components_rest_field' );
+
+/**
  * Preload the Active BuddyPress Components.
  *
  * @since 8.0.0
@@ -71,7 +133,7 @@ add_action( 'bp_blocks_init', __NAMESPACE__ . '\bp_register_block_components', 1
  * @param string[] $paths The Block Editors preload paths.
  * @return string[] The Block Editors preload paths.
  */
-function bp_block_preload_paths( $paths = array() ) {
+function bp_blocks_preload_paths( $paths = array() ) {
 	return array_merge(
 		$paths,
 		array(
@@ -79,4 +141,4 @@ function bp_block_preload_paths( $paths = array() ) {
 		)
 	);
 }
-add_filter( 'block_editor_rest_api_preload_paths', __NAMESPACE__ . '\bp_block_preload_paths' );
+add_filter( 'block_editor_rest_api_preload_paths', __NAMESPACE__ . '\bp_blocks_preload_paths' );
