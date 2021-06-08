dc66f603634e4c0db3e475276a643fc4869c48e3
imath <math.viet@gmail.com>
2021-06-08 15:26:00 +0200

    Add a function to get the logged in user
    Improve the inline documentation

--- src/bp-core/js/block-assets/data/utils.js
+++ src/bp-core/js/block-assets/data/utils.js
@@ -20,6 +20,15 @@ const {
  */
 import BP_CORE_STORE_KEY from './register';
 
+/**
+ * Checks whether a component or the feature of an active component is enabled.
+ *
+ * @since 8.0.0
+ *
+ * @param {string} component (required) The component to check.
+ * @param {string} feature (optional) The feature to check.
+ * @return {boolean} Whether a component or the feature of an active component is enabled.
+ */
 export function isActive( component, feature = '' ) {
 	const components = useSelect( ( select ) => {
 		return select( BP_CORE_STORE_KEY ).getActiveComponents();
@@ -32,6 +41,21 @@ export function isActive( component, feature = '' ) {
 	}
 
 	return get( activeComponent, [ 'features', feature ] );
+}
+
+/**
+ * Returns the logged in user object.
+ *
+ * @since 8.0.0
+ *
+ * @return {Object} The logged in user object.
+ */
+export function loggedInUser() {
+	const loggedInUser = useSelect( ( select ) => {
+		return select( 'core' ).getCurrentUser();
+	}, [] );
+
+	return loggedInUser;
 }
 
 export default isActive;
