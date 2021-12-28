8a7364a047b8085c9ed4144a443e4429e8e36420
imath <math.viet@gmail.com>
2021-12-28 20:19:54 +0100

    Add attributes to hide the label or use an Icon for the search button

--- src/add-ons/bp-search-block/js/search-form/edit.js
+++ src/add-ons/bp-search-block/js/search-form/edit.js
@@ -16,6 +16,7 @@ const {
 	},
 	components: {
 		PanelBody,
+		ToggleControl,
 	},
 	element: {
 		createElement,
@@ -44,7 +45,16 @@ import SearchOptionsRadioGroup from './options-radiogroup';
 
 const editSearchFormBlock = ( { attributes, setAttributes } ) => {
 	const blockProps = useBlockProps();
-	const { activeOptions, placeholder, action, label, buttonText, defaultOption } = attributes;
+	const {
+		activeOptions,
+		placeholder,
+		action,
+		label,
+		useLabel,
+		buttonText,
+		useIcon,
+		defaultOption,
+	} = attributes;
 
 	const setAvailableOptions = ( option, isChecked ) => {
 		let newOptions = activeOptions;
@@ -90,18 +100,46 @@ const editSearchFormBlock = ( { attributes, setAttributes } ) => {
 				<PanelBody title={ __( 'Search options', 'bp-search-block' ) } initialOpen={ true }>
 					{ searchSettings }
 				</PanelBody>
+				<PanelBody title={ __( 'Display options', 'bp-search-block' ) } initialOpen={ false }>
+					<ToggleControl
+						label={ __( 'Show the search label', 'bp-search-block' ) }
+						checked={ !! useLabel }
+						onChange={ () => {
+							setAttributes( { useLabel: ! useLabel } );
+						} }
+						help={
+							useLabel
+								? __( 'Display a label over the search field.', 'bp-search-block' )
+								: __( 'Toggle to display a label over the search field.', 'bp-search-block' )
+						}
+					/>
+					<ToggleControl
+						label={ __( 'Use a Search Icon', 'bp-search-block' ) }
+						checked={ !! useIcon }
+						onChange={ () => {
+							setAttributes( { useIcon: ! useIcon } );
+						} }
+						help={
+							useIcon
+								? __( 'Use a search icon instead of the search button text.', 'bp-search-block' )
+								: __( 'Toggle to use a search icon instead of the search button text.', 'bp-search-block' )
+						}
+					/>
+				</PanelBody>
 			</InspectorControls>
 			<div { ...blockProps }>
 				<form action={ action } method="post">
-					<RichText
-						tagname="label"
-						className="bp-search-label"
-						aria-label={ __( 'Label text', 'bp-search-block' ) }
-						placeholder={ __( 'Add label…', 'bp-search-block' ) }
-						withoutInteractiveFormatting
-						value={ label }
-						onChange={ ( html ) => setAttributes( { label: html } ) }
-					/>
+					{ useLabel && (
+						<RichText
+							tagname="label"
+							className="bp-search-label"
+							aria-label={ __( 'Label text', 'bp-search-block' ) }
+							placeholder={ __( 'Add label…', 'bp-search-block' ) }
+							withoutInteractiveFormatting
+							value={ label }
+							onChange={ ( html ) => setAttributes( { label: html } ) }
+						/>
+					) }
 					<div className="bp-block-search__inside-wrapper">
 						<input
 							type="search"
@@ -109,15 +147,27 @@ const editSearchFormBlock = ( { attributes, setAttributes } ) => {
 							name="search-terms"
 							placeholder={ placeholder }
 						/>
-						<RichText
-							tagname="button"
-							className="wp-block-search__button bp-search-button"
-							aria-label={ __( 'Button text', 'bp-search-block' ) }
-							placeholder={ __( 'Add text…', 'bp-search-block' ) }
-							withoutInteractiveFormatting
-							value={ buttonText }
-							onChange={ ( html ) => setAttributes( { buttonText: html } ) }
-						/>
+
+						{ useIcon && (
+							<button
+								type="button"
+								className="wp-block-search__button bp-search-button bp-block-search__icon-button has-icon"
+							>
+								<div class="bp-search-block-icon" />
+							</button>
+						) }
+
+						{ ! useIcon && (
+							<RichText
+								tagname="button"
+								className="wp-block-search__button bp-search-button"
+								aria-label={ __( 'Button text', 'bp-search-block' ) }
+								placeholder={ __( 'Add text…', 'bp-search-block' ) }
+								withoutInteractiveFormatting
+								value={ buttonText }
+								onChange={ ( html ) => setAttributes( { buttonText: html } ) }
+							/>
+						) }
 					</div>
 					<SearchOptionsRadioGroup
 						selected={ defaultOption }
